20d4086ae5e3a6436c1b2dff0043c11a7e486239
Moatez <kenani.moatez@gmail.com>
2023-07-04 14:47:20 +0100

    delete all token

--- app.py
+++ app.py
@@ -1,85 +1,48 @@
-from flask import Flask , request , jsonify , make_response , render_template , session
-import jwt
-from datetime import datetime , timedelta
-from functools import wraps
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '6bb14f2e41bb41209aae287dc1a9f063'
+app.secret_key = 'your_secret_key'
 
-def token_required(func):
-    @wraps(func)
-    def decorated(*args,**kwargs):
-        token = request.args.get('token')
-        if not token:
-            return jsonify({'Alert!':'Token is missing'})
-        try:
-            payload =jwt.decode(token, app.config ['SECRET_KEY'])
-        except:
-            return jsonify({'Alert!':'Invalid Token'})
+# Simulated user database (replace with your own user retrieval logic)
+users = {'user1': {'password': 'password1'},
+         'user2': {'password': 'password2'}}
 
-    return decorated
 
-#home
 @app.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return'Logged in currently!'
+    return 'Welcome to the home page!'
 
-#for public
-@app.route('/public')
-def public():
-    return 'For Public'
-#Authentificated
-@app.route('/auth')
-@token_required
-@token_required
-def auth():
-    try:
-        token = request.args.get('token')
-        payload = jwt.decode(token, app.config['SECRET_KEY'])
-        return 'JWT is verified. Welcome to your dashboard!'
-    except jwt.ExpiredSignatureError:
-        return 'Token has expired. Please obtain a new token.'
-    except jwt.InvalidTokenError:
-        return 'Invalid token. Authentication failed.'
 
-#login
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.form['username'] and request.form['password'] == '123456':
-        token = jwt.encode({
-            'user': request.form['username'],
-            'expiration': str(datetime.utcnow() + timedelta(seconds=120))
-        }, app.config['SECRET_KEY'])
-        return jsonify({'token': token.decode('utf-8')})
-    else:
-        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm:"Authentication Failed!"'})
+    if 'username' in session:
+        return redirect(url_for('dashboard'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and password == users[username]['password']:
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return 'Invalid username or password'
+
+    return render_template('login.html')
 
 
-@app.route('/refresh-token', methods=['POST'])
-@token_required
-def refresh_token():
-    try:
-        token = request.args.get('token')
-        payload = jwt.decode(token, app.config['SECRET_KEY'])
+@app.route('/dashboard')
+def dashboard():
+    if 'username' not in session:
+        return redirect(url_for('login'))
 
-        # Generate a new token with refreshed expiration time
-        refreshed_token = jwt.encode({
-            'user': payload['user'],
-            'expiration': str(datetime.utcnow() + timedelta(seconds=120))
-        }, app.config['SECRET_KEY'])
-
-        return jsonify({'token': refreshed_token.decode('utf-8')})
-
-    except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'Token has expired. Please login again.'}), 401
-
-    except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid token. Authentication failed.'}), 401
+    return 'Welcome to the dashboard!'
 
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
-if __name__ == "__main__" :
+
+if __name__ == '__main__':
     app.run(debug=True)
