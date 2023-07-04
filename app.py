3391bffa1d5355bb394a0944ea24e0c938e9911c
Moatez <kenani.moatez@gmail.com>
2023-07-04 14:20:33 +0100

    add a refresh token

--- app.py
+++ app.py
@@ -34,23 +34,51 @@ def public():
 #Authentificated
 @app.route('/auth')
 @token_required
+@token_required
 def auth():
-    return 'JWT is verified. welcome to your dashboard!'
+    try:
+        token = request.args.get('token')
+        payload = jwt.decode(token, app.config['SECRET_KEY'])
+        return 'JWT is verified. Welcome to your dashboard!'
+    except jwt.ExpiredSignatureError:
+        return 'Token has expired. Please obtain a new token.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Authentication failed.'
 
 #login
 @app.route('/login', methods=['POST'])
-def lgoin():
-    if request.form['username'] and request.form['password']=='123':
-        session['logged_in'] = True
+def login():
+    if request.form['username'] and request.form['password'] == '123456':
         token = jwt.encode({
-            'user':request.form['username'],
-            'expiration':str(datetime.utcnow() + timedelta(seconds=60))
-        },
-            app.config['SECRET_KEY'])
-        return jsonify({'token': token})
-
+            'user': request.form['username'],
+            'expiration': str(datetime.utcnow() + timedelta(seconds=120))
+        }, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('utf-8')})
     else:
-        return make_response('Unable to verify', 403, {'WWW-Authentificate' : 'Basic realm:"Authentification Failed !'})
+        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm:"Authentication Failed!"'})
+
+
+@app.route('/refresh-token', methods=['POST'])
+@token_required
+def refresh_token():
+    try:
+        token = request.args.get('token')
+        payload = jwt.decode(token, app.config['SECRET_KEY'])
+
+        # Generate a new token with refreshed expiration time
+        refreshed_token = jwt.encode({
+            'user': payload['user'],
+            'expiration': str(datetime.utcnow() + timedelta(seconds=120))
+        }, app.config['SECRET_KEY'])
+
+        return jsonify({'token': refreshed_token.decode('utf-8')})
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({'message': 'Token has expired. Please login again.'}), 401
+
+    except jwt.InvalidTokenError:
+        return jsonify({'message': 'Invalid token. Authentication failed.'}), 401
+
 
 
 if __name__ == "__main__" :
